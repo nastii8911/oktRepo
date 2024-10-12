b91a235b6fde745998135adfe1d234ae84e38acb
nastii8911 <nastii891@gmail.com>
2024-10-12 13:29:26 +0300

    описание classList

---
+++ classList/classList.js
@@ -0,0 +1,26 @@
+"use strict";
+const myElement = document.getElementById("myElement");
+// Добавить класс
+myElement.classList.add("highlight");
+// Удалить класс
+myElement.classList.remove("box");
+// Переключить класс
+myElement.classList.toggle("active");
+// Проверить наличие класса
+if (myElement.classList.contains("active")) {
+    console.log("Element is active");
+}
+//  Вопрос: Почему в этом случае null?
+// document?.addEventListener("DOMContentLoaded", () => {
+//   const myElement = document.getElementById("myElement");
+//   // Добавить класс
+//   myElement?.classList.add("highlight");
+//   // Удалить класс
+//   myElement?.classList.remove("box");
+//   // Переключить класс
+//   myElement?.classList.toggle("active");
+//   // Проверить наличие класса
+//   if (myElement?.classList.contains("active")) {
+//     console.log("Element is active");
+//   }
+// });
