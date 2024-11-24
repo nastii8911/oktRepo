3673a82bc93becb25cb10e2a6f71abc0a2370407
artur <example@email.com>
2024-11-24 15:08:40 +0500

    class Person

--- toDo-22.10/связь Person.js
+++ toDo-22.10/связь Person.js
@@ -1,19 +1,19 @@
 "use strict";
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 class Person {
     constructor({ name, age }) {
         this.name = name;
         this.age = age;
         this.connections = [];
+        this.id = getRandomInRange(1, 100000);
     }
-    addConnections(person) {
-        if (person instanceof Person) {
-            if (!this.connections.includes(person)) {
-                this.connections.push(person); // Добавляем связь
-                person.connections.push(this); // Двусторонняя связь
-            }
-        }
-        else {
-            throw new Error("Connection must be an instance of Person");
+    addConnection(person) {
+        const connectionsIds = this.connections.map((conn) => conn.id);
+        if (!connectionsIds.includes(person.id)) {
+            this.connections.push(person);
+            person.connections.push(this);
         }
     }
     getConnections() {
@@ -24,6 +24,6 @@ const ivan = new Person({ name: "Ivan", age: 30 });
 const peter = new Person({ name: "Petr", age: 26 });
 console.log(ivan.getConnections()); // []
 console.log(peter.getConnections()); // []
-ivan.addConnections(peter);
+ivan.addConnection(peter);
 console.log(ivan.getConnections()); // ["Petr"]
 console.log(peter.getConnections()); // ["Ivan"]
--- toDo-22.10/связь Person.ts
+++ toDo-22.10/связь Person.ts
@@ -1,26 +1,30 @@
+function getRandomInRange(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class Person {
-  name: string;
-  age: number;
-  connections: Person[];
+  public id: number;
+  public name: string;
+  public age: number;
+  public connections: Person[];
 
   constructor({ name, age }: { name: string; age: number }) {
     this.name = name;
     this.age = age;
     this.connections = [];
+    this.id = getRandomInRange(1, 100000);
   }
 
-  addConnections(person: Person): void {
-    if (person instanceof Person) {
-      if (!this.connections.includes(person)) {
-        this.connections.push(person); // Добавляем связь
-        person.connections.push(this); // Двусторонняя связь
-      }
-    } else {
-      throw new Error("Connection must be an instance of Person");
+  public addConnection(person: Person): void {
+    const connectionsIds = this.connections.map((conn) => conn.id);
+
+    if (!connectionsIds.includes(person.id)) {
+      this.connections.push(person);
+      person.connections.push(this);
     }
   }
 
-  getConnections(): string[] {
+  public getConnections(): string[] {
     return this.connections.map((conn) => conn.name);
   }
 }
@@ -31,7 +35,7 @@ const peter = new Person({ name: "Petr", age: 26 });
 console.log(ivan.getConnections()); // []
 console.log(peter.getConnections()); // []
 
-ivan.addConnections(peter);
+ivan.addConnection(peter);
 
 console.log(ivan.getConnections()); // ["Petr"]
 console.log(peter.getConnections()); // ["Ivan"]
