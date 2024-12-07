101155c1253f47858ef90c2e88fe25aa03ba16d3
nastii891 <nastii891@gmail.com>
2024-12-07 08:56:34 +0300

    jsonServer

---
+++ jsonServer/fetchServerJson.ts
@@ -0,0 +1,18 @@
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json", // Указываем, что отправляем данные в формате JSON
+  },
+  body: JSON.stringify({
+    name: "Nina",
+    email: "Nina@example.com",
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log("User added:", data))
+  .catch((error) => console.error("Error:", error));
+
+fetch("http://localhost:3000/users")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
